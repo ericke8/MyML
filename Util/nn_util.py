f7254c30158932b85b6cbb00c30de6fef033f6cd
Eric Ke <26385966+ericke8@users.noreply.github.com>
2018-01-20 18:49:18 -0800

    add  def generator_to_mini_batches; syn on naming convention

--- Util/nn_util.py
+++ Util/nn_util.py
@@ -1,9 +1,9 @@
 import math
 import numpy as np
 
-def iter_mini_batches(X, Y, mini_batch_size = 64):
+def mini_batch_generator(X, Y, mini_batch_size = 64):
     """
-    Creates an iterable of random minibatches from (X, Y), caller usage: "for xyBatch in iter_mini_batches(X, Y, 64):"
+    Creates an generator of random minibatches from (X, Y), caller usage: "for xBatch, yBatch in iter_mini_batches(X, Y, 64):"
     
     Arguments:
     X -- input data, of shape (number of examples, Height, Width, Channel, ...), as long as number of example is X.shape[0] 
@@ -22,10 +22,37 @@ def iter_mini_batches(X, Y, mini_batch_size = 64):
     
     # use yield keyword to create the generator for caller's iteration
     for start_index in range(0, m - mini_batch_size + 1, mini_batch_size):
-        miniBatch = indices[start_index:start_index + mini_batch_size]
-        yield X[miniBatch], Y[miniBatch]
+        mini_batch = indices[start_index:start_index + mini_batch_size]
+        yield X[mini_batch], Y[mini_batch]
 
-def iter_mini_batches(X, Y, mini_batch_size = 64, , seed = 0):
+def generator_to_mini_batches(items, mini_batch_size):
+    """
+    Generate a list of mini batches of size mini_batch_size.
+    Some time it is desirable to convert from a generator to slited list.
+    Arguments:
+    items -- a generator 
+    mini_batch_size -- size of each mini bacth
+    Output: A list of lists, each with a size of mini_batch_size, except maybe the the mini batch.
+    """
+    mini_batches = []
+    mini_batch = []
+    batch_count = 0
+    for item in items:
+        batch_count += 1
+        if batch_count == batch_size:
+            mini_batch.append(item)
+            mini_batches.append(miniBatch)
+            mini_batch = []
+            batch_count = 0
+        else:            
+            mini_batch.append(item)
+    # last mini batch has a odd size (leaa than mini_batch_size), but still need to appeded to the batches
+    if batch_count != 0:
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
+
+def iter_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     """
     Creates a list of random minibatches from (X, Y)
     
@@ -33,7 +60,7 @@ def iter_mini_batches(X, Y, mini_batch_size = 64, , seed = 0):
     X -- input data, of shape (number of examples, Height, Width, Channels), number of example is X.shape[0] 
     Y -- ground truth "labels" of shape (number of examples, label). number of example is Y.shape[0]  
     mini_batch_size - size of the desired mini-batches, integer
-    seed -- In case you need a repeatable random result, user can pass random number in if true random is desired.
+    seed -- In case you need a repeatable mini_batch for debugging, user can pass random seed in if desired.
     
     Returns:
     mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
@@ -67,17 +94,17 @@ def iter_mini_batches(X, Y, mini_batch_size = 64, , seed = 0):
     return mini_batches
 
 
-def convert_to_one_hot(Y, numberOfClasses):
+def convert_to_one_hot(Y, number_classes):
     """
     Creates a one hot version of matrix Y 
     
     Arguments:
     Y -- typically just a one dimension "label" vector (containing just the actual class number)
-    numberOfClasses -- numberOfClasses determines the size of I matrix to create for each label.
+    number_classes -- numberOfClasses determines the size of I matrix to create for each label.
     
     Returns:
     Y -- the one hot version matrix of original Y vector of shape [numberOfLabels, numberOfClasses]
     """
-    Y = np.eye(numberOfClasses)[Y.reshape(-1)]
+    Y = np.eye(number_classes)[Y.reshape(-1)]
     return Y
 
